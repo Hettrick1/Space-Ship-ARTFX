43fad0ceb2b32538da569eb317d04012e32a428d
Hettrick1 <evann.jacquot@gmail.com>
2024-02-05 20:13:03 +0100

    [add] x rotation + different cameras

--- Assets/RotationController.cs
+++ Assets/RotationController.cs
@@ -4,22 +4,66 @@ public class RotationController : MonoBehaviour
 {
     [SerializeField]private float rotationAnglez = 1;
     [SerializeField] private float rotationAngley = 2;
+    [SerializeField] private float rotationAnglex = 2;
     private float rotationSpeed = 1f;
     [SerializeField] private float speed = 40.0f;
 
+    [SerializeField] private Camera camera1;
+    [SerializeField] private Camera camera2;
+    [SerializeField] private bool isChangingCamera;
+
+    private void Start()
+    {
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space) && !isChangingCamera)
+        {
+            ChangeCamera();
+        }
+
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * Time.deltaTime;
 
+        Quaternion xRotation = CalculateRotationQuaternion(rotationAnglex * input.y, Vector3.right);
         Quaternion yRotation = CalculateRotationQuaternion(rotationAngley * input.x, Vector3.up);
         Quaternion zRotation = CalculateRotationQuaternion(rotationAnglez * -input.x, Vector3.forward);
 
-        print(zRotation);
-
-        transform.localRotation = yRotation * transform.localRotation * zRotation;
+        if(input.x != 0)
+        {
+            transform.localRotation = yRotation * transform.localRotation * zRotation;
+        }
+        if (input.y != 0)
+        {
+            transform.localRotation = xRotation * transform.localRotation;
+        }
              
 
         transform.position += transform.forward * Time.deltaTime * speed;
+    }
+
+    void ChangeCamera()
+    {
+        isChangingCamera = true;
+        if (camera1.isActiveAndEnabled)
+        {
+            camera1.enabled = false;
+            camera2.enabled = true;
+            Invoke(nameof(ResetIsChangingCamera), 0.5f);
+        }
+        else
+        {
+            camera1.enabled = true;
+            camera2.enabled = false;
+            Invoke(nameof(ResetIsChangingCamera), 0.5f);
+        }
+    }
+
+    void ResetIsChangingCamera()
+    {
+        isChangingCamera = false;
     }
 
     Quaternion CalculateRotationQuaternion(float angle, Vector3 axis)
